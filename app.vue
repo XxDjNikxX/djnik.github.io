e3d71c6efd8df095e7721f744bb246529d1bcdc5
DjNik <nikitos1235@gmail.com>
2025-01-25 20:43:46 +0300

    feat: new test site

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <section
+    class="relative bg-[url(https://i.redd.it/tm69aost3sea1.jpg)] bg-cover bg-center bg-no-repeat"
+  >
+
+  
+    <div class="relative flex h-dvh lg:items-center lg:px-5">
+      <div class="py-20 relative px-20 border-2 border-dashed border-color-black mx-auto mr-5">
+        <h1 class="font-body text-5xl font-extrabold text-right text-white">
+          Hey!
+        </h1>
+        <p class="mt-5 font-body max-w-rg sm:text-xl/relaxed text-right text-white">
+          Welcome to my site :)
+        </p>
+        <p class="mt-5 font-body max-w-lg sm:text-xl/relaxed text-right text-slate-100">
+          Honestly, i am Junior DevOps Engineer with experience in Linux server administration, Docker container development and writing playbooks for Ansible.
+        </p>
+        <p class="mt-5 font-body max-w-lg sm:text-xl/relaxed text-right text-white">
+          But wrote this page purely for fun. 
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
